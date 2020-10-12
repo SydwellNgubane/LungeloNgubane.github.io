2c7e4c9cbe943796f00c749f671f5fea5e0e48c9
Lungelo Ngubane <sydwellngubane@gmail.com>
2020-10-12 07:28:29 +0200

    Hosting final

---
+++ newCV/mainContentJavaScript.js
@@ -0,0 +1,33 @@
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+/*window.onscroll = function () { myFunction() };
+
+    var navbar = document.getElementById("navbar");
+    var sticky = navbar.offsetTop;
+
+    function myFunction() {
+        if (window.pageYOffset >= sticky) {
+            navbar.classList.add("sticky")
+        } else {
+            navbar.classList.remove("sticky");
+        }
+        
+    } <button onclick="topFunction()" id="myBtn" title="Go to top">Home</button>
+
+
+*/
